1e3d4f1bebdadbc09d72727670f954a413c84942
Reece-2008 <75809279+Reece-2008@users.noreply.github.com>
2021-05-03 14:52:44 -0400

    Update jokebot.js

--- jokebot.js
+++ jokebot.js
@@ -1,16 +1,19 @@
+
 function startJoke(){
   if(
 confirm("Wanna hear a joke?")
   ){
-  prompt(" What did the orange name it's biograhpy?");
+  response= prompt(" What did the orange name it's biograhpy?");
+if(answer.toLowerCase() === response.toLowerCase()) {
+alert("You got it ") 
+} else{
+ alert(" PULP NON FICTION!!");
+}
 
-alert(" PULP NON FICTION!!");
-
+  
 }
 }
-startJoke()
+
 var response;
 var answer = "Pulp Non Fiction";
-if (answer === response.toLowerCase()) {
-alert("You got it ")
-}
+
